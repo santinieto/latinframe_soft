2af5e499c827eb3ce03a9d73a34c0c67c3318880
Santiago Enrique Nieto <santi.nieto@live.com>
2023-10-15 17:16:10 -0300

    Agrego el - a los comandos y corrijo potenciales bugs y errores de tipeo '

--- tests/test_channel.py
+++ tests/test_channel.py
@@ -4,44 +4,44 @@ from src.db import Database
 
 class TestVideo(unittest.TestCase):
 
-    # def test_create_channel_object(self):
-    #     try:
-    #         channel_obj = Channel()
-    #     except Exception as e:
-    #         self.fail(f"Could not create Channel object.\n\tError value: {str(e)}")
+    def test_create_channel_object(self):
+        try:
+            channel_obj = Channel()
+        except Exception as e:
+            self.fail(f"Could not create Channel object.\n\tError value: {str(e)}")
 
-    # def test_channel_1(self):
-    #     channel = Channel(id='UCK1i2UviaXLUNrZlAFpw_jA')
-    #     channel.fetch_channel_data()
-    #     channel.fetch_videos_data()
-    #     print(str(channel))
+    def test_channel_1(self):
+        channel = Channel(id='UCK1i2UviaXLUNrZlAFpw_jA')
+        channel.fetch_channel_data()
+        channel.fetch_videos_data()
+        print(str(channel))
 
-    # def test_channel_2(self):
-    #     channel = Channel(id='UCu60hbJGH8nlCSFwfVe_wwA')
-    #     channel.fetch_channel_data()
-    #     print(str(channel))
+    def test_channel_2(self):
+        channel = Channel(id='UCu60hbJGH8nlCSFwfVe_wwA')
+        channel.fetch_channel_data()
+        print(str(channel))
 
-    # def test_channel_load_videos_ids(self):
-    #     channel = Channel(id='UCu60hbJGH8nlCSFwfVe_wwA')
-    #     channel.fetch_channel_data()
+    def test_channel_load_videos_ids(self):
+        channel = Channel(id='UCu60hbJGH8nlCSFwfVe_wwA')
+        channel.fetch_channel_data()
 
-    #     # Get channel videos ID from DB
-    #     with Database() as db:
-    #         query = 'SELECT VIDEO_ID FROM VIDEO WHERE CHANNEL_ID = ?'
-    #         params = (channel.id,)
-    #         db_ids = db.select(query, params)
-    #         db_ids = [item[0] for item in db_ids]
+        # Get channel videos ID from DB
+        with Database() as db:
+            query = 'SELECT VIDEO_ID FROM VIDEO WHERE CHANNEL_ID = ?'
+            params = (channel.id,)
+            db_ids = db.select(query, params)
+            db_ids = [item[0] for item in db_ids]
 
-    #     # Show scrapped videos and videos obtained from DB
-    #     print('Scrapped videos:', len(channel.video_ids_list))
-    #     print('Videos from DB:', len(db_ids))
+        # Show scrapped videos and videos obtained from DB
+        print('Scrapped videos:', len(channel.video_ids_list))
+        print('Videos from DB:', len(db_ids))
 
-    #     # Combine lists
-    #     total_id_list = channel.video_ids_list + db_ids
-    #     channel.video_ids_list = list(set(total_id_list))
+        # Combine lists
+        total_id_list = channel.video_ids_list + db_ids
+        channel.video_ids_list = list(set(total_id_list))
 
-    #     # Show final IDs list
-    #     print('Final ID list len:', len(channel.video_ids_list))
+        # Show final IDs list
+        print('Final ID list len:', len(channel.video_ids_list))
 
     def test_channel_load_channel_ids(self):
         import os
