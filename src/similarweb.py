ddfbee252ee3206c84d8da2b454b2c2cd67fe9e8
Santiago Enrique Nieto <santi.nieto@live.com>
2023-10-17 00:59:18 -0300

    0037: Agregue la funcion para obtener la informacion de una web de similarweb (WIP)

--- src/similarweb.py
+++ src/similarweb.py
@@ -31,6 +31,59 @@ def get_similarweb_table(filepath=''):
 
     return data_list
 
+def get_similarweb_web_info(filepath='scraped_page.html'):
+
+    #########################################
+    # Abro el archivo
+    #########################################
+    with open(filepath, 'r', encoding="utf-8") as file:
+        page = BeautifulSoup(file, 'html.parser')
+
+    #########################################
+    # Analizo la informacion de rankeo
+    #########################################
+    # Obtengo el cuadro de encabezado
+    rank_header = page.find('div', class_='wa-rank-list wa-rank-list--md')
+    # Obtengo las cajas
+    global_box = rank_header.find('div', class_='wa-rank-list__item wa-rank-list__item--global')
+    country_box = rank_header.find('div', class_='wa-rank-list__item wa-rank-list__item--country')
+    category_box = rank_header.find('div', class_='wa-rank-list__item wa-rank-list__item--category')
+    # Obtengo los valores de las cajas
+    global_rank = global_box.find('p', class_='wa-rank-list__value').text
+    country_rank = country_box.find('p', class_='wa-rank-list__value').text
+    category_rank = category_box.find('p', class_='wa-rank-list__value').text
+
+    #########################################
+    # Analizo la informacion de rankeo
+    #########################################
+    # Encuentro la caja de de engagement
+    engagement_box = page.find('div', class_='engagement-list')
+    # Encuentro los elementos dentro de la caja
+    engagement_elements = engagement_box.find_all('div', class_='engagement-list__item')
+    # Para cada elemento, obtengo el dato asociado
+    total_visits = engagement_elements[0].find('p', class_='engagement-list__item-value').text
+    bounce_rate = engagement_elements[1].find('p', class_='engagement-list__item-value').text
+    pages_per_visit = engagement_elements[2].find('p', class_='engagement-list__item-value').text
+    avg_duration_visit = engagement_elements[3].find('p', class_='engagement-list__item-value').text
+
+    #########################################
+    # Armo el diccionario de datos
+    #########################################
+    web_data = {
+        'global_rank': global_rank,
+        'country_rank': country_rank,
+        'category_rank': category_rank,
+        'total_visits': total_visits,
+        'bounce_rate': bounce_rate,
+        'pages_per_visit': pages_per_visit,
+        'avg_duration_visit': avg_duration_visit,
+    }
+
+    return web_data
+
 if __name__ == '__main__':
     data = get_similarweb_table('scraped_page.html')
+    print(data)
+
+    data = get_similarweb_web_info('scraped_page.html')
     print(data)
