3a6c1234e48df9a71fa78f68c75f2b764fec8496
Santiago Enrique Nieto <santi.nieto@live.com>
2023-10-27 00:35:20 -0300

    0078: Agregue la carpeta img. Mejore algunos plots

--- src/db_clean.py
+++ src/db_clean.py
@@ -192,7 +192,6 @@ if __name__ == '__main__':
 
     # Realizar un inner join utilizando el ID como clave de referencia
     df = pd.merge(df_1, df_2, on='CHANNEL_ID', how='left')
-    print(df)
 
     # Convertir la columna 'UPDATE_DATE' a objetos de fecha
     df['UPDATE_DATE'] = pd.to_datetime(df['UPDATE_DATE'])
@@ -204,43 +203,60 @@ if __name__ == '__main__':
 
     # Relleno los posibles valores que sean 0
     sort_columns = ['CHANNEL_ID','UPDATE_DATE']
-    valid_columns = ['SUBSCRIBERS']
+    valid_columns = ['SUBSCRIBERS', 'DAILY_SUBS','MONTHLY_SUBS','TOTAL_VIEWS','VIDEOS_COUNT']
     df = replace_zeros_with_nearest_valid(df=df, sort_columns=sort_columns, valid_columns=valid_columns)
 
     # Obtengo la lista de canales
     channel_ids = df['CHANNEL_ID'].unique()
 
     # Para cada canal voy a hacer unos graficos
-    for channel_id in channel_ids[:1]:
+    for channel_id in channel_ids:
         df_filt = df[ df['CHANNEL_ID'] == channel_id ]
-        print(df_filt)
 
-        try:
-            # Obtengo el nombre del canal
-            channel_name = df_filt['CHANNEL_NAME'].tolist()[0]
-        except:
-            continue
+        # Obtengo el nombre del canal
+        channel_name = df_filt['CHANNEL_NAME'].tolist()[0]
 
         # Convertir la columna 'UPDATE_DATE' a objetos de fecha
         df_filt['UPDATE_DATE'] = pd.to_datetime(df_filt['UPDATE_DATE'])
 
-        # Crear un gráfico
-        plt.figure()
+        # Crear una figura con un grid de 2x2
+        fig, axs = plt.subplots(2, 2, figsize=(12, 8))
 
-        # Graficar los datos
-        plt.plot(df_filt['UPDATE_DATE'], df_filt['SUBSCRIBERS'], 'bo-', markersize=5)
+        # Cantidad de videos subidos
+        axs[0, 0].plot(df_filt['UPDATE_DATE'], df_filt['VIDEOS_COUNT'], 'bo-', markersize=5)
+        axs[0, 0].xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%y'))
+        axs[0, 0].tick_params(axis='x', rotation=90)
+        axs[0, 0].grid()
+        axs[0, 0].set_xlabel('Fecha')
+        axs[0, 0].set_ylabel('Videos subidos')
+        axs[0, 0].set_title(f'Videos subidos a lo largo del tiempo')
 
-        # Configurar el formato de las fechas en el eje x
-        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%y'))
+        # Histograma de suscripciones diarias
+        axs[0, 1].hist(df_filt['DAILY_SUBS'])
+        axs[0, 1].grid()
+        axs[0, 1].set_ylabel('Ocurrencias')
+        axs[0, 1].set_xlabel('Suscripciones diarias')
+        axs[0, 1].set_title(f'Histograma de suscripciones diarias')
 
-        # Rotar las etiquetas del eje x
-        plt.xticks(rotation=90)
+        # Gráfico de visualizaciones/suscripciones totales
+        axs[1, 0].plot(df_filt['UPDATE_DATE'], df_filt['TOTAL_VIEWS'], 'bo-', markersize=5)
+        axs[1, 0].xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%y'))
+        axs[1, 0].tick_params(axis='x', rotation=90)
+        axs[1, 0].grid()
+        axs[1, 0].set_xlabel('Fecha')
+        axs[1, 0].set_ylabel('Visualizaciones')
+        axs[1, 0].set_title(f'Visualizaciones a lo largo del tiempo')
 
-        # Mostrar la cuadrícula
-        plt.grid()
-        plt.xlabel('Fecha')
-        plt.ylabel('Suscripciones')
-        plt.title(f'Suscripciones a lo largo del tiempo\nCanal: {channel_name}')
+        axs[1, 1].plot(df_filt['UPDATE_DATE'], df_filt['SUBSCRIBERS'], 'ro-', markersize=5)
+        axs[1, 1].xaxis.set_major_formatter(mdates.DateFormatter('%d-%b-%y'))
+        axs[1, 1].tick_params(axis='x', rotation=90)
+        axs[1, 1].grid()
+        axs[1, 1].set_xlabel('Fecha')
+        axs[1, 1].set_ylabel('Suscripciones')
+        axs[1, 1].set_title(f'Suscripciones a lo largo del tiempo')
+
+        # Agregar un título común en el medio arriba
+        fig.suptitle(f'Estadísticas de {channel_name}', fontsize=16)
 
     # Muestro el plot
     plt.show()
